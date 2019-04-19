9cc225a65765866caa4073c51cc26cc677d1e9ba
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-04-18 20:34:51 -0700

    fetch is partially working

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 'use strict';
-
+console.log('js connected');
 const apiKey = /*your API key here*/
 
 // const searchURL = 'https://newsapi.org/v2/everything';
 
 
 function formatQueryParams(params) {
+  console.log('format ran');
   const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryItems.join('&');
 }
 
-function displayResults(responseJson, maxResults) {
+function displayResults(responseJson) {
   // if there are previous results, remove them
   console.log(responseJson);
   $('#results-list').empty();
   // iterate through the articles array, stopping at the max number of results
-  for (let i = 0; i < responseJson.articles.length & i<maxResults ; i++){
-    // for each video object in the articles
-    //array, add a list item to the results 
-    //list with the article title, source, author,
-    //description, and image
+  for (let i = 0; i < responseJson.length; i++){
+    
     $('#results-list').append(
       `<li><h3><a href="${responseJson.articles[i].url}">${responseJson.articles[i].title}</a></h3>
       <p>${responseJson.articles[i].source.name}</p>
@@ -33,40 +31,30 @@ function displayResults(responseJson, maxResults) {
   $('#results').removeClass('hidden');
 };
 
-function getNews(query, maxResults=10) {
-  const params = {
-    q: query,
-    language: "en",
-  };
-  const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
+function getRepos(handle) {
+ 
+  const searchUrl = `https://api.github.com/users/${handle}/repos`;
+  console.log(searchUrl);
 
-  console.log(url);
-
-  const options = {
-    headers: new Headers({
-      "X-Api-Key": apiKey})
-  };
-
-  fetch(url, options)
+  fetch(searchUrl)
     .then(response => {
       if (response.ok) {
         return response.json();
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => displayResults(responseJson, maxResults))
+    .then(responseJson => displayResults(responseJson))
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
-}
+};
 
 function watchForm() {
   $('form').submit(event => {
  event.preventDefault();
-    const searchTerm = $('#js-search-term').val();
-    const maxResults = $('#js-max-results').val();
-    getNews(searchTerm, maxResults);
+ console.log('watchform ran');
+    const githubHandle = $('#js-search-term').val();
+    getRepos(githubHandle);
   });
 }
 
